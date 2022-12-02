dd2e227898f57f2d5234250217aa37084c7d30ca
Vincent WENGLER <vincentwengler97@gmail.com>
2022-12-02 11:48:52 +0100

    Add TODO commentaire

--- EventPlanner/Program.cs
+++ EventPlanner/Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// TODO Don't forget injection ;)
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
